b1ec357ecb32904f0bbd125775f2f2dec53826cf
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-02-05 09:33:59 +0100

    update

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://a0ce-197-253-32-226.ngrok-free.app/api'; // Replace with your backend URL
 
 export const getProducts = async () => {
-    const response = await axios.get(`${API_URL}/products/products`);
+    const response = await axios.get(`https://a0ce-197-253-32-226.ngrok-free.app/api/products/products`);
     return response.data;
 };
 
