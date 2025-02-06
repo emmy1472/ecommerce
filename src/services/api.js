78f211c3a7f2caaaf2601a8aa05ab656223f3425
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-02-06 15:44:07 +0100

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
+// services/api.js
 import axios from 'axios';
 
-const API_URL = 'https://f1ec-197-253-32-226.ngrok-free.app/api'; // Replace with your backend URL
-
 export const getProducts = async () => {
-    const response = await axios.get(`${API_URL}/products/products`);
-    return response.data;
+  try {
+    const response = await axios.get("https://bb18-197-253-32-226.ngrok-free.app/api/products/products/");
+    //console.log("API Response:", response.data); // Debugging log
+    return response.data; // Axios directly returns the data from the response
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
 };
-
